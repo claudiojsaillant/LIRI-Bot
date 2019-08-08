b05139d0428e9eee6248f21feea979af115089b4
Cloud <claudiojsaillant@gmail.com>
2019-08-08 16:30:51 -0400

    readme edit

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# LIRI-Bot
+### LIRI-Bot
+
+-Don't you always wanted to search for music, concerts and movies at the same time? That's not going to be a problem anymore thanks to LIRI, a command line app.
+
+## App Composition
+
+This app is organized in 4 files:
+
+1. keys.js: It takes the spotify authentication keys from a .env file.
+
+2. .env: this is where you are going to put your spotify keys in this format
+
+SPOTIFY_ID=''
+SPOTIFY_SECRET=''
+
+3. package.json: Contains the name and version of all the npm packages that will be required to make the app work.
+
+4. liri.js: The app that is going to run in command line interface with node, this file makes the api calls and display the data in an organized way.
+
+## Technologies used
+
+#JavaScript
+#Node.js
+ Node Packages:
+    *npm
+    *moment.js
+    *axios
+    *spotify api
+    *file system
+
+## Deployed link: 
+
+## Instructions
+
+1) Install node.js and the packages that need to be installed (found in package.json).
+
+2) There are 4 commands that can use used:
+    
+    spotify-this: Search for an specific song and displays the artist, song name, preview link and the album from the song. example call: node liri.js spotify-this "old town road".
+
+    concert-this: Search for concerts of an specific artist and displays detailed information from concerts (the place, the city and the date) if the artitst is on tour. example call: node liri.js  concert-this "metallica".
+
+    movie-this: Search for an specific movie and displays information about it (title, year, ratings, plot, etc..). example call: node liri.js movie-this "The Avengers"
+
+    do-what-it-says: Takes a .txt file in the same directory as liri.js, reads it and searches a command call the user writes in it (the command call should be one of the 3 commands stated). example call: node liri.js do-what-it-says name.txt
+    the command call should be inside the .txt file, theres an example .txt file in the app directory 
+     './random.txt'
+
+
+3) Open command line interface and type: 
+    node liri.js "command-here" "argument-here"
+
+4) Enjoy the app.
+
+
+
--- liri.js
+++ liri.js
@@ -1,3 +1,104 @@
-require("dotenv").config();
+require("dotenv").config({ path: '.env' });
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
+var moment = require("moment")
 var spotify = new Spotify(keys.spotify);
+var fs = require('fs');
+
+if (process.argv[2] != 'do-what-it-says' && process.argv[3] != undefined) {
+    botAction(process.argv[2], process.argv[3])
+} else if (process.argv[3] != undefined) {
+    filename = process.argv[3];
+    doThis(filename)
+}
+else {
+    console.log("Make sure that you fill the next parameter with the name of the text file && make sure the file is inside the directory of the bot")
+}
+
+function spotifyMe(argument) {
+    spotify
+        .request("https://api.spotify.com/v1/search?q=" + argument + "&type=track")
+        .then(function (data) {
+            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Preview link: " + data.tracks.items[0].preview_url);
+            console.log("Album: " + data.tracks.items[0].album.name)
+        })
+        .catch(function (err) {
+            console.error('Error occurred: ' + err);
+        });
+}
+
+function concertMe(argument) {
+    axios({
+        method: 'get',
+        url: "https://rest.bandsintown.com/artists/" + argument + "/events?app_id=codingbootcamp"
+    })
+        .then(function (response) {
+            eventData = response.data
+            if (response.data.length != 0) {
+                for (i = 0; i < response.data.length; i++) {
+                    console.log(eventData[i].venue.name);
+                    console.log(eventData[i].venue.city + ", " + eventData[i].venue.country);
+
+                    console.log("Date: " + moment(eventData[i].datetime).format('l'))
+                    console.log("=================================");
+                }
+            }
+            else {
+                console.log("No events upcoming for this artist")
+            }
+        });
+}
+
+function movieMe(argument) {
+    var url;
+    if (argument === undefined) {
+        url = 'http://www.omdbapi.com/?apikey=trilogy&t=Mr+Nobody'
+    } else {
+        url = 'http://www.omdbapi.com/?apikey=trilogy&t=' + argument
+    }
+    axios({
+        method: 'get',
+        url: url
+    })
+        .then(function (response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Produced Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        });
+}
+
+function doThis(filename) {
+    fs.readFile(filename, 'utf8', function (err, data) {
+        if (err) throw err;
+        textData = data.split(',')
+        textData1 = textData[1]
+        if (textData[0] === 'concert-this') {
+            textData1 = textData1.split('"');
+            textData1 = textData1[1];
+        }
+        botAction(textData[0], textData1)
+    })
+}
+
+function botAction(action, argument) {
+    switch (action) {
+        case 'spotify-this-song':
+            spotifyMe(argument);
+            break;
+        case 'concert-this':
+            concertMe(argument);
+            break;
+        case 'movie-this':
+            movieMe(argument);
+            break;
+        default: console.log("Invalid command");
+    }
+}
